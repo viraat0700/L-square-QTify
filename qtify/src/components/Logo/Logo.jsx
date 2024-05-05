51f632cda5dc067d05e60a865094e91950fa4d78
viraat0700 <viraat.shrivastava0700@gmail.com>
2024-05-05 16:41:24 +0530

    changed the logo , placeholder and Poppins

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -2,5 +2,9 @@ import React from "react";
 import logo from "../../assets/logo.png";
 
 export default function Logo() {
-  return <img src={logo} width={67} alt="qtrip logo" />;
+  return (
+    <div>
+      <img src={logo} width={67} alt="qtrip logo" />
+    </div>
+  );
 }
--- qtify/src/components/SearchBar/SearchBar.jsx
+++ qtify/src/components/SearchBar/SearchBar.jsx
@@ -31,7 +31,7 @@ export default function SearchBar({ data }) {
             </InputAdornment>
           ),
         }}
-        placeholder="Search for songs"
+        placeholder="search"
         name="search"
         fullWidth
         onChange={handleChange}
